2df69aef45d15818ef4c66fc5fcb41da203e1edf
MELISSA OLAS <melissa.olas@gmail.com>
2025-03-01 15:57:46 +0100

    adding webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
-{
-  module.exports = {
-    target: "node",
-  };
-}
+const path = require("path");
+const serverConfig = {
+  target: "node",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "lib.node.js",
+  },
+  //…
+};
+
+const clientConfig = {
+  target: "web", // <=== can be omitted as default is 'web'
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "lib.js",
+  },
+  //…
+};
+
+module.exports = [serverConfig, clientConfig];
