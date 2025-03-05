22c5c3214f42d291d85a52ff1ce74d4989099e4f
MELISSA OLAS <melissa.olas@gmail.com>
2025-03-05 21:48:09 +0100

    modify authmidleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,18 +1,24 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header("Authorization");
+  const authHeader = req.header("Authorization");
 
-  if (!token) {
+  if (!authHeader) {
     return res.status(401).json({ error: "Accès refusé, aucun token fourni" });
   }
 
+  const token = authHeader.split(" ")[1]; // Récupère uniquement le token après "Bearer"
+
+  if (!token) {
+    return res.status(401).json({ error: "Format du token invalide" });
+  }
+
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Vérifie et décode le token
-    req.user = decoded; // Ajoute l'utilisateur décodé dans `req.user`
-    next(); // Passe à la prochaine fonction middleware
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
   } catch (err) {
-    res.status(400).json({ error: "Token invalide" });
+    res.status(403).json({ error: "Token invalide ou expiré" });
   }
 };
 
