9f2a3d758ad85cfb5781e913ccf2e60280b9af96
MELISSA OLAS <melissa.olas@gmail.com>
2025-04-16 14:40:43 +0200

    test

--- src/pages/admin/login/TestLogin.tsx
+++ src/pages/admin/login/TestLogin.tsx
@@ -28,7 +28,7 @@ const Login: React.FC = () => {
       const apiUrl = process.env.REACT_APP_API_URL!;
       // || "http://localhost:3001"
       const cleanApiUrl = apiUrl.endsWith("/") ? apiUrl.slice(0, -1) : apiUrl;
-      console.log("API_URL utilisée:", cleanApiUrl);
+      console.log("API_URL utilisée:", process.env.REACT_APP_API_URL!);
 
       const response = await axios.post<LoginResponse>(`${cleanApiUrl}/login`, {
         email,
